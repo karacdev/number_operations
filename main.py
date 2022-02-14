44453be89efc5a843d8f7dae1ce4c454bb45dde6
karaCDev <72567414+karacdev@users.noreply.github.com>
2022-02-14 16:42:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+age = 24 #this is an interger--which is a while number.and
+pi = 3.14159 #this is a float--which is a number with a deciaml point.
+
+
+math_operation = 1 + 3 * 4 / 2 - 2
+print(math_operation)
+
+#note: division always returns a float number in python--example:
+
+float_division = 12 / 3
+print(float_division)
+
+#to get rid of the float number:
+
+interger_division = 12 // 3
+print(interger_division)
+
+#what this does is performs the division & removes the decimal place.
